7099501f7bee08c2a4fd6e41c9c05ab0ec10b2df
jadiazg02 <joseadiazg02@correo.ugr.es>
2017-01-08 21:54:33 +0100

    Finalizados primeros modelos. Comenzamos arboles decisión. closes #2.

---
+++ arbolesdecision.R
@@ -0,0 +1,72 @@
+"En el anterior script obteniamos maualmente las reglas para asignar a ciertos grupos
+de población la clase buscada. Esto podemos hacerlo mecanicamente con un árbol de decision"
+
+install.packages('rattle')
+install.packages('rpart.plot')
+install.packages('RColorBrewer')
+library(rattle)
+library(rpart.plot)
+library(RColorBrewer)
+library(rpart)
+
+"En R, tenemos rpart para dibujar nuestros arboles, pero podemos hacer uso de otras 
+librerias externas que nos daran más juego."
+
+fit <- rpart(Survived ~ Pclass + Sex + Age + SibSp + Parch + Fare + Embarked,
+             data=titanic2,
+             method="class")
+
+plot(fit)
+text(fit)
+
+fancyRpartPlot(fit)
+
+
+
+"En base a este algoritmo vamos a hacer una predicción."
+
+Prediction <- predict(fit, titanicTest, type = "class")
+
+submit <- data.frame(PassengerId = titanicTest$PassengerId, Survived = Prediction)
+
+write.csv(submit, file = "/Users/joseadiazg/Documents/Knime-WorkSpace/MachineLearning_Disaster_TID/output/arbol.csv", row.names = FALSE)
+
+#ACCURACY 0.77033
+
+"Parece que no mejora nuestro modelo manual, por lo que vamos a dejar solo las variables
+que nuestro proceso de seleccion de variables nos dijo que era imporante"
+
+fit <- rpart(Survived ~ Pclass + Sex + Age + SibSp + Fare,
+             data=titanic2,
+             method="class")
+
+Prediction <- predict(fit, titanicTest, type = "class")
+
+submit <- data.frame(PassengerId = titanicTest$PassengerId, Survived = Prediction)
+
+write.csv(submit, file = "/Users/joseadiazg/Documents/Knime-WorkSpace/MachineLearning_Disaster_TID/output/arbol2.csv", row.names = FALSE)
+
+"Mejoramos pero como podemos ver aún no es lo suficiente para aumentar la precisión de nuestro anterior modelo."
+
+#ACCURACY 0.77990
+
+
+
+fit <- rpart(Survived ~ Pclass + Sex + Age + SibSp + Parch + Fare + Embarked,
+             data=titanic2,
+             method="class", 
+             control=rpart.control(minsplit=2, cp=0))
+
+fancyRpartPlot(fit)
+
+"Si analizamos lo que hemos realizado hemos eliminado las variables de control
+por lo que el algoritmo obtiene una regla para cada uno de los distintos pasajeros.
+Obteniendo un 100% de acc en training pero sobreaprendiendo en test."
+
+
+Prediction <- predict(fit, titanicTest, type = "class")
+
+submit <- data.frame(PassengerId = titanicTest$PassengerId, Survived = Prediction)
+
+write.csv(submit, file = "/Users/joseadiazg/Documents/Knime-WorkSpace/MachineLearning_Disaster_TID/output/arboloverfit.csv", row.names = FALSE)
+
--- notebook.R
+++ notebook.R
@@ -1,3 +1,79 @@
+#Análisis exploratorio (seleccion de caracteristicas) y regresion logistica entre variables en R
+
+"Primero haremos una limpieza y estudio exploratorio de los datos de los datos. 
+
+Tener datos limpios y de buena calidad es un factor importante antes de comenzar a
+trabajar con nuestro data set. Uno de los principales problemas a descubrir y solventar
+son los valores perdidos, sobre esta cuestion será la primera que trabajaremos."
+
+#Cargamos el dataset y asignamos un valor concreto a los valores perdidos
+
+titanic <- read.csv("/Users/joseadiazg/Documents/Knime-WorkSpace/MachineLearning_Disaster_TID/dataset/train.csv", header = T, na.strings=c(""))
+titanicTest <- read.csv("/Users/joseadiazg/Documents/Knime-WorkSpace/MachineLearning_Disaster_TID/dataset/test.csv", header = T, na.strings=c(""))
+
+#Mostramos los datos, podemos comprobar como los vacios salen marcados como NA
+
+head(titanic)
+
+#Vamos a ver la distribución de los missing values y la variabilidad de las variables para entender mejor los datos. 
+
+sapply(titanic,function(x) sum(is.na(x)))
+
+#También podemos visualizarlo gráficamente
+
+install.packages("Amelia")
+library(Amelia)
+missmap(titanic, main = "Missing values vs observed")
+
+"Vemos como hay 177 con la edad perdida, 687 con la cabina vacia y 2 con el embarque.
+Estos puntos ya son útiles, ya que aplicando simple lógica podemos ver que los valores perdidos
+en la edad serán un factor importante y decisivo ya que acorde a datos historicos en el titanic
+niños y mujeres tuvieron preferencia en el acceso a los botes."
+
+#Vamos a estudiar la variabilidad
+
+sapply(titanic, function(x) length(unique(x)))
+
+
+
+"Directamente vemos que tenemos muchas variables que no nos sirven por lo que podemos 
+seleccionar solo algunas, a priori, eliminamremos las que sabemos a ciencia cierta 
+que no sirven para nada. Vemos claramente que:
+
+-PassengerId es solo un id, pues su variabilidad coincide con el numero de muestras en el dataset.
+-Con el nombre sucede exactamente lo mismo. 
+-Cabin es clara candidata a ser eliminada pues su cantidad de valores perdidos la hacen inservible.
+-Ticket es el número de ticket por lo que tampoco es relevante por lo que las eliminaremos."
+
+
+titanic2 <- titanic[,-c(1,4, 9, 11)]
+head(titanic2)
+
+
+"Nos hemos quitado el problema de los missing values en Cabin, pero seguimos teniendo problemas 
+con la variable edad y esta como hemos visto antes no es menester de quitarla. Una posible solución es
+asignar a los valores perdidos la media de los demás que si conocemos, el más común, la mediana...."
+
+titanic2$Age[is.na(titanic2$Age)] <- mean(titanic2$Age,na.rm=T)
+
+
+#Respecto a embarked eliminaremos las dos filas que llevan un valor perdido. 
+
+titanic2 <- titanic2[!is.na(titanic2$Embarked),]
+rownames(titanic2) <- NULL
+
+
+
+#Entrenamos el modelo
+
+model <- glm(Survived ~.,family=binomial(link='logit'),data=titanic2)
+
+summary(model)
+
+#Aplicamos el test anova 
+
+anova(model, test="Chisq")
+
 "En base al estudio realizado anteriormente, nos centraremos en la variables que el
 estudio de regresion nos ofreció, es decir: Sex, Age y Pclass"
 
@@ -102,5 +178,7 @@ aggregate(Survived ~ Child + Fare2 + Pclass + Sex, data=titanic2, FUN=function(x
 "Aunque parececia que anteriormente no nos ofreceria mejores resultados, ahora hemos conseguido
 'salvar' algunos hombres al fin, por lo que nuestro modelo nos ofrece ya mejores resultados."
 
+
 #TERCER MODELO ACCURACY 0.78947
+
 
