92be9405b17f312fe1fd7fcf0a8b378db6b99e2f
jadiazg02 <joseadiazg02@correo.ugr.es>
2017-04-16 14:05:10 +0200

    Comenzada aproximación final. avanza #8

---
+++ final-approach/bestrandomforest.R
@@ -0,0 +1,29 @@
+# Cargamos librerias 
+
+suppressMessages(library(caret))                                 # Machine learning
+suppressMessages(library(rpart))
+suppressMessages(library(e1071))
+suppressMessages(library(randomForest))
+suppressMessages(library(mlbench))
+suppressMessages(library(party))
+suppressMessages(library(ggplot2))                               # Plotting
+suppressMessages(library(lattice))
+suppressMessages(library(grid))
+suppressMessages(library(gridExtra, warn.conflicts = FALSE))
+suppressMessages(library(ggrepel))
+suppressMessages(library(vcd))
+suppressMessages(library(rpart.plot))
+suppressMessages(library(rattle))
+suppressMessages(library(ROCR))
+suppressMessages(library(mice))                                  # Data imputation
+suppressMessages(library(xtable))                                # Pretty printing dataframes
+suppressMessages(library(plyr, warn.conflicts = FALSE))          # Manipulating dataframes
+suppressMessages(library(Hmisc))
+suppressMessages(library(Amelia))                                # Missing data
+suppressMessages(library(dplyr, warn.conflicts = FALSE))
+suppressMessages(library(stringr))  
+
+
+#Obtenemos los datos
+
+
